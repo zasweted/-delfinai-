3e9bbebe1178980a4630487776786979631a7be0
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-13 16:39:29 +0300

    rand

---
+++ 001/app.js
@@ -0,0 +1,11 @@
+console.log('asdfasdf');
+
+function rand(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+};
+
+const arr = [...Array(10)].map(_ => rand(7, 77));
+
+console.log(arr);
