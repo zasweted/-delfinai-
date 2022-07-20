3902c269bb68a496bf7b108ac4b2d56cc8f4c767
Aleksandr Vasiljev <zaswet@gmail.com>
2022-07-20 14:28:15 +0300

    react

--- r37/src/App.jsx
+++ r37/src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css';
 import Kosmosas from './components/005/kosmosas';
 
+const mas = ['Pilkis', 'Murkis', 'Gagarinas'];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Labas</h1>
+
+        {
+          mas.map((a, i)=> <h2 key={i}>{a}</h2>)
+        }
+
         <Kosmosas spalva ="crimson" krastas="40px"></Kosmosas>
       </header>
     </div>
---
+++ r37/src/components/005/Kn.jsx
@@ -0,0 +1,8 @@
+// function Kn() {
+
+//     return ();
+
+
+// };
+
+// export default Kn;
